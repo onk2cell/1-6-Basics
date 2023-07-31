3a0fa4378aece1b368e588a9a1592b345bb20bf9
onk2cell <116635851+onk2cell@users.noreply.github.com>
2023-08-01 00:26:15 +0530

    Add files via upload

--- demo1.py
+++ demo1.py
@@ -1,9 +1,36 @@
-x = 10
-y = 10
-# id returns memory location
-print(id(x))
-print(id(y))
-x = 20
-print("=================")
-print(id(x))
-print(id(y))
+import os
+import csv
+
+class Leagger():
+    def __init__(self,amount,type,account,name,note,holder,debtcred):
+        self.amount = input("Enter the amount")
+        self.type = input("Enter the type")
+        self.account = input("account type ")
+        self.name = input("name of giver")
+        self.note = input("anyspecial note")
+        self.holder = input("name of taker")
+        self.debtcred = input("given or taken")
+    
+
+class createnew(Leagger):
+    def __init__(self, amount, type, account, name, note, holder, debtcred,filename):
+        self.filename = input("Enter the file name you want to store the data")
+        super().__init__(amount, type, account, name, note, holder, debtcred)
+
+    def write_to_csv(self):
+        with open(self.filename, 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.holder, self.account, self.name, self.note, self.debtcred, self.amount])
+
+        
+
+
+one = createnew("200","pos","bank","onkar","pinetable","sangita","ced","SONA")
+one.write_to_csv()
+
+
+
+
+
+
+
